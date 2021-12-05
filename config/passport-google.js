849c66da96c8ebbc91e1ccfd24dd5cac9cbab201
omssma <omaralkhanbashi92@gmail.com>
2021-12-05 06:15:37 -0500

    Added passport.js

---
+++ config/passport-google.js
@@ -0,0 +1,22 @@
+const passport = require("passport");
+const googleStrategy = require("passport-google-oauth").OAuth2Strategy;
+const User = require("../models/User");
+
+passport.use(
+  new googleStrategy(
+    {
+      clientID: // Using Google API
+        "110257807412-3eb2jq1ag3b4387ahoqgur6lb923l65t.apps.googleusercontent.com", //Here Enter your client ID
+      clientSecret: "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", //Here Enter your Client Secret
+      callbackURL: "http://localhost:3001/users/auth/google/callback",
+      passReqToCallback: true,
+    },
+
+    function(accessToken, refreshToken, profile, done) {
+      User.findOrCreate({ googleId: profile.id }, function (err, user) {
+        return done(err, user);
+      });
+ }
+));
+
+module.exports = passport;
--- server.js
+++ server.js
@@ -3,9 +3,12 @@ const routes = require('./controllers');
 const sequelize = require('./config/connection');
 const exphbs = require('express-handlebars');
 const hbs = exphbs.create({});
+const passport= require('passport');
 const path = require('path');
 const session = require('express-session');
+const router = require('./controllers');
 
+require('./config/passport-google');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -24,19 +27,45 @@ const sess = {
 
 app.use(session(sess));
 
-
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
 
-app.use(express.static(path.join(__dirname, 'public')));
+// Passport authentication using google strategy
+app.get('/', (req, res) => {
+  res.send('<a href="/auth/google">Authenticate with Google</a>');
+});
+
+// Authenticate user's google credentials
+app.get('/auth/google',
+  passport.authenticate('google', { scope: [ 'email', 'profile' ] }
+));
+
+// Callback function
+app.get( '/auth/google/callback',
+  passport.authenticate( 'google', {
+    successRedirect: '/protected',
+    failureRedirect: '/auth/google/failure'
+  })
+);
+
+function isLoggedIn(req, res, next) {
+  req.user ? next() : res.sendStatus(401);
+}
+
+app.get('/protected', isLoggedIn, (req, res) => {
+  res.send(`Hello ${req.user.displayName}`);
+});
+
 
 // turn on routes
-app.use(routes);
+app.use(router);
 
 // turn on connection to db and server
 sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () => console.log('Now listening'));
 });
+
